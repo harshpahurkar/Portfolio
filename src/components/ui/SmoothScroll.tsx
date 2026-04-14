73b50e2381813e6d28c22cacbe0bf641a77d05cc
harshpahurkar <fatjockjimmmy@gmail.com>
2026-04-14 22:51:48 +0530

    fix: switch Lenis to lerp-based scroll for snappy smooth feel

--- src/components/ui/SmoothScroll.tsx
+++ src/components/ui/SmoothScroll.tsx
@@ -2,22 +2,17 @@
 
 import { useEffect } from "react";
 import Lenis from "lenis";
+import "lenis/dist/lenis.css";
 
 export default function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.6,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.1,
       smoothWheel: true,
       touchMultiplier: 2,
-      wheelMultiplier: 0.8,
+      wheelMultiplier: 1,
+      autoRaf: true,
     });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
 
     // Make lenis available for anchor links
     const handleClick = (e: MouseEvent) => {
